0380fca8d5e03b5adcd69f3a3d75514a3ae1760b
Administrator <Administrator@windows10.microdone.cn>
2020-09-08 17:23:39 +0800

    20200908_3

--- mystock/RuleData/RuleScore.py
+++ mystock/RuleData/RuleScore.py
@@ -29,7 +29,16 @@ selected_rule.drop(["type","id","rulename"], axis = 1, inplace = True)
 selected_rule["sequence"] = selected_rule["grade"].groupby(selected_rule["date"]).rank(ascending=0, method="first")
 check_data = selected_rule[["date","grade","sequence","kpis", "num_kpis","dapan"]]
 
-check_data.to_csv(filedir + "rule_score_py.csv", index = False)
+
+selected_rule["ret1"] = (selected_rule["next1"] - selected_rule["price"]) / selected_rule["price"] 
+selected_rule["ret2"] = (selected_rule["next2"] - selected_rule["price"]) / selected_rule["price"] 
+selected_rule["ret3"] = (selected_rule["next3"] - selected_rule["price"]) / selected_rule["price"] 
+selected_rule["ret4"] = (selected_rule["next4"] - selected_rule["price"]) / selected_rule["price"] 
+
+
 
 selected = selected_rule[selected_rule["sequence"] <= 2]
 unselected = selected_rule[selected_rule["sequence"] > 2]
+
+a = selected[["ret1","ret2","ret3","ret4"]].mean()
+b = unselected[["ret1","ret2","ret3","ret4"]].mean()
